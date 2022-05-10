bdf405b7e8880479824494e7e4c77146cc1b3e82
Arun Jilla <arunjilla4356@gmail.com>
2022-05-10 20:44:16 +0530

    initial commit

---
+++ src/components/button/button.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import devices from '../media/devices';
+
+export const CustomButton = styled.button`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  margin: 2%;
+  font-size: 15px;
+  background-color: #d81b60;
+  color: white;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+
+  @media ${devices.mobile} {
+    font-size: 8px;
+    margin: 0;
+    padding: 0;
+  }
+`;
---
+++ src/components/products/products.styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.main`
+  display: flex;
+`;
+
+export const CategoriesContainer = styled.div`
+background-color: lightgray;
+font-family: fangsong;
+padding: 13px 15px 13px 16px;
+
+
+  div {
+    margin: 0 5px 5px 5px;
+    padding-bottom: 2px;
+    border-bottom: 1px solid gray;
+    cursor: pointer;
+  }
+`;
+
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 20px;
+  row-gap: 50px;
+`; 
